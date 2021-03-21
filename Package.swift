522b4d25394718c13cce3ade3f207c839896c13e
Omar Estrella <oyestrella91@gmail.com>
2021-03-21 19:10:44 -0400

    Update packages

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ var packageDependencies: [Package.Dependency] = [
   .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", "4.0.0" ..< "5.0.0"),
   .package(url: "https://github.com/malcommac/Hydra.git", "2.0.0" ..< "3.0.0"),
   .package(url: "https://github.com/sushichop/Puppy.git", "0.1.0" ..< "1.0.0"),
-  .package(url: "https://github.com/tesseract-one/Starscream.git", .branch("master")),
 ]
 
 let package = Package(
@@ -25,16 +24,11 @@ let package = Package(
       name: "friend-bot",
       dependencies: [
         "Sword",
-        "CommonCrypto",
         .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
         .product(name: "Hydra", package: "Hydra"),
         .product(name: "Puppy", package: "Puppy"),
       ], path: "Sources"
     ),
-    .target(
-      name: "CommonCrypto",
-      path: "Sources/CommonCrypto",
-      linkerSettings: [ .linkedLibrary("common") ]),
     .testTarget(
       name: "friend-botTests",
       dependencies: ["friend-bot"]
